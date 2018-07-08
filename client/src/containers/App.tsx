88de6a40a664967731aa9df64b1393511cc1b869
Kyle Roach <kroach.work@gmail.com>
2018-07-08 11:42:40 -0400

    Setup frontend

---
+++ client/src/containers/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.p`
+  color: pink;
+`
+
+const App = () => <Title>You Are Awesome</Title>
+
+export default App
