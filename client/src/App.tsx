4679c411f00f3c72cd7daa3c1313c45cf6cd8a6e
Kyle Roach <kroach.work@gmail.com>
2018-07-08 15:42:15 -0400

    Connect graphql

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
 
 import { SiteWrapper } from './components'
 import { Applications } from './pages'
+import client from './config/graphql'
 
 const App = () => (
-  <Router>
-    <SiteWrapper>
-      <Switch>
-        <Route exact path="/" component={Applications} />
-      </Switch>
-    </SiteWrapper>
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <SiteWrapper>
+        <Switch>
+          <Route exact path="/" component={Applications} />
+        </Switch>
+      </SiteWrapper>
+    </Router>
+  </ApolloProvider>
 )
 
 export default App
--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -3,3 +3,10 @@ declare module '*.png'
 declare module '*.jpg'
 
 declare module 'tabler-react'
+
+declare var process: {
+  env: {
+    NODE_ENV: string
+    REACT_APP_GRAPH_ENDPOINT: string
+  }
+}
