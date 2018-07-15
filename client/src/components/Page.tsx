564c30a7de37f9fc82c7bf0cc77736cdf8cdf865
Kyle Roach <kroach.work@gmail.com>
2018-07-15 02:41:07 -0400

    Add Page transition

--- client/src/components/Page.tsx
+++ client/src/components/Page.tsx
@@ -2,30 +2,44 @@ import React from 'react'
 import styled from 'styled-components'
 import { Theme } from '../config'
 
-const Header = styled.header`
-  padding-top: 20px;
-  padding-bottom: 20px;
+const Header = styled.header.attrs({
+  className: 'py-6',
+})`
   background: #fcfcfd;
   border-bottom: 1px solid ${Theme.border};
 `
 
-const Content = styled.div`
-  margin-top: 30px;
+const Container = styled.div`
+  animation: appear 400ms;
+
+  @keyframes appear {
+    0% {
+      opacity: 0;
+      transform: translateY(10px);
+    }
+
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `
 
 interface Props {
   renderHeader?: React.ReactNode
 }
 
-const Page: React.SFC<Props> = props => (
-  <main>
-    {props.renderHeader && (
-      <Header>
-        <div className="container">{props.renderHeader}</div>
-      </Header>
-    )}
+const Page: React.SFC<Props> = ({ renderHeader, children }) => (
+  <main className="relative">
+    <Container>
+      {renderHeader && (
+        <Header>
+          <div className="container">{renderHeader}</div>
+        </Header>
+      )}
 
-    <Content className="container">{props.children}</Content>
+      <div className="container mt-6">{children}</div>
+    </Container>
   </main>
 )
 
